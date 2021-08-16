e56110c7481f71eae4bfdfc017a5b09ab9b2a7b7
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-08-16 12:58:48 -0600

    Update plot_data.m

--- RF/Applications/AWR Optimization/plot_data.m
+++ RF/Applications/AWR Optimization/plot_data.m
@@ -1,5 +1,5 @@
 % Load data
-filename = "data.ddf";
+filename = "scan_3053_4x100_16-Aug-2021 1158.ddf";
 ddf = DDFIO;
 ddf.load(filename);
 
@@ -24,8 +24,8 @@ figure(4)
 hold off
 smithplot(G, 'LineStyle', ':', 'Marker', '+')
 
-figure(5);
-hold off
-plot(f, Z_vi);
-hold on;
-plot(f, G2Z(G, 50));
+% figure(5);
+% hold off
+% plot(f, Z_vi);
+% hold on;
+% plot(f, G2Z(G, 50));
