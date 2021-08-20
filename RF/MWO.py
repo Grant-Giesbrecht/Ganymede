4021a31db2ff90f2f22256fcb321f32bcdbd5828
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-08-20 14:59:25 -0600

    MWO: Added way to init goals quickly

--- RF/MWO.py
+++ RF/MWO.py
@@ -1,5 +1,6 @@
 import time
 from pyddf import *
+import win32com.client
 
 # mwOptGoalType
 mwOGT_Equals = 0
@@ -43,29 +44,29 @@ def printOptVars(awrde, indent="", rel_indent="  "):
 		print(f"{indent}{rel_indent}: {awrde.Project.Optimizer.Variables.Item(idx).Step}")
 
 def printGoals(awrde, indent="", rel_indent="  "):
-    ''' Print all optimization goals in the MWO object `awrde`. '''
+	''' Print all optimization goals in the MWO object `awrde`. '''
 
-    for idx in range(1, awrde.Project.OptGoals.Count + 1):
-        print(f"{indent}Goal {idx}:")
-        printGoal(awrde, idx, f"{indent}{rel_indent}")
+	for idx in range(1, awrde.Project.OptGoals.Count + 1):
+		print(f"{indent}Goal {idx}:")
+		printGoal(awrde, idx, f"{indent}{rel_indent}")
 
 def printGoal(awrde, idx, indent=""):
-    '''Print the goal in index `idx` in the MWO object `awrde`. '''
+	'''Print the goal in index `idx` in the MWO object `awrde`. '''
 
-    print(f"{indent}Circuit: {awrde.Project.OptGoals.Item(idx).CircuitName}")
-    print(f"{indent}Cost: {awrde.Project.OptGoals.Item(idx).Cost}")
-    print(f"{indent}Enable: {awrde.Project.OptGoals.Item(idx).Enable}")
-    print(f"{indent}LVal: {awrde.Project.OptGoals.Item(idx).LVal}")
-    print(f"{indent}Measurement: {awrde.Project.OptGoals.Item(idx).Measurement}")
-    print(f"{indent}Measurement Name: {awrde.Project.OptGoals.Item(idx).MeasurementName}")
-    print(f"{indent}Name: {awrde.Project.OptGoals.Item(idx).Name}")
-    print(f"{indent}Tag: {awrde.Project.OptGoals.Item(idx).Tag}")
-    print(f"{indent}Type: {awrde.Project.OptGoals.Item(idx).Type}")
-    print(f"{indent}Weight: {awrde.Project.OptGoals.Item(idx).Weight}")
-    print(f"{indent}xStart: {awrde.Project.OptGoals.Item(idx).xStart}")
-    print(f"{indent}xStop: {awrde.Project.OptGoals.Item(idx).xStop}")
-    print(f"{indent}yStart: {awrde.Project.OptGoals.Item(idx).yStart}")
-    print(f"{indent}yStop: {awrde.Project.OptGoals.Item(idx).yStop}")
+	print(f"{indent}Circuit: {awrde.Project.OptGoals.Item(idx).CircuitName}")
+	print(f"{indent}Cost: {awrde.Project.OptGoals.Item(idx).Cost}")
+	print(f"{indent}Enable: {awrde.Project.OptGoals.Item(idx).Enable}")
+	print(f"{indent}LVal: {awrde.Project.OptGoals.Item(idx).LVal}")
+	print(f"{indent}Measurement: {awrde.Project.OptGoals.Item(idx).Measurement}")
+	print(f"{indent}Measurement Name: {awrde.Project.OptGoals.Item(idx).MeasurementName}")
+	print(f"{indent}Name: {awrde.Project.OptGoals.Item(idx).Name}")
+	print(f"{indent}Tag: {awrde.Project.OptGoals.Item(idx).Tag}")
+	print(f"{indent}Type: {awrde.Project.OptGoals.Item(idx).Type}")
+	print(f"{indent}Weight: {awrde.Project.OptGoals.Item(idx).Weight}")
+	print(f"{indent}xStart: {awrde.Project.OptGoals.Item(idx).xStart}")
+	print(f"{indent}xStop: {awrde.Project.OptGoals.Item(idx).xStop}")
+	print(f"{indent}yStart: {awrde.Project.OptGoals.Item(idx).yStart}")
+	print(f"{indent}yStop: {awrde.Project.OptGoals.Item(idx).yStop}")
 
 
 def runOptimizer(awrde):
@@ -233,9 +234,13 @@ def sweepdict_to_ddf(dl):
 	return ddf
 
 class OptGoal:
+	''' This class describes an optimizer goal for AWR. The objective is not to
+	list the optimizer goals defined in a project, but to describe a goal in
+	python and apply those goals fresh to AWR.'''
 
 	def __init__(self):
 
+		# Human readable name for easier referencing (optional)
 		self.rdname = ""
 
 		self.xStart = -1
@@ -248,7 +253,58 @@ class OptGoal:
 		self.w = 1
 		self.circSimName = "" #Example: "SweepSchema_4950_8x100.AP_HB"
 		self.measName = "" #Example: "PT(PORT_2)", "PAE(PORT_1, PORT_2)"
-		self.comparison = MWO.mwOGT_Equals #Options: mwOGT_GreaterThan, mwOGT_LessThan, mwOGT_Equals
+		self.comparison = mwOGT_Equals #Options: mwOGT_GreaterThan, mwOGT_LessThan, mwOGT_Equals
+
+	def set(self, x0=None, x1=None, xU=None, y0=None, y1=None, yU=None, L=None, w=None, circSim=None, measName=None, comparison=None, rdname=None):
+		''' Set one or more parameters in one line '''
+
+		if x0 is not None:
+			self.xStart = x0
+		if x1 is not None:
+			self.xStop = x1
+		if xU is not None:
+			self.xUnit = xU
+		if y0 is not None:
+			self.yStart = y0
+		if y1 is not None:
+			self.yStop = y1
+		if yU is not None:
+			self.yUnit = yU
+		if L is not None:
+			self.L = L
+		if w is not None:
+			self.w = w
+		if circSim is not None:
+			self.circSimName = circSim
+		if measName is not None:
+			self.measName = measName
+		if comparison is not None:
+			self.comparison = comparison
+		if rdname is not None:
+			self.rdname = rdname
+
+
+class AWRVariable:
+	''' Describes a variable/equation in AWR. The goal is to locate an equation
+	or variable already defined in an AWR schematic, global definition, etc and
+	allow the script to easily adjust its value, enable optimization, etc. '''
+
+	def __init__(self, schematic, name):
+
+		# Human readable name for easier referencing
+		self.rdname
+
+
+		self.name = "" # Name (as appears in 'Parameter' list)
+		self.schema = "" # Schematic in which it is defined
+
+		self.maximum = None
+		self.minimum = None
+		self.constrained = None
+		self.optEnabled = None
+
+		self.found = False
+
 
 class AWRProject:
 
@@ -257,34 +313,34 @@ class AWRProject:
 		self.awrde = None
 		self.cs = "-> " # CLI message symbol
 
+		# Variables described in Python to find and optimize in the AWR project
+		self.variables = []
+
+		# Opt goals defined in Python to apply to the AWR project
 		self.optGoals = []
 
 	def connectFindSchema(self, schema_name):
 
 		# Initialize connection to Microwave Office
-		awrde = win32com.client.Dispatch("MWOApp.MWOffice")
+		self.awrde = win32com.client.Dispatch("MWOApp.MWOffice")
 
 		# Check that schematic exists
-		if not awrde.Project.Schematics.Exists(schema_name):
+		if not self.awrde.Project.Schematics.Exists(schema_name):
 			print(f"Cannot find required schematic '{schema_name}'.\n\nAborting.")
 			sys.exit()
 		else:
 			print(f"{self.cs}Found schematic '{schema_name}'.")
-
-		return awrde
 
 	def applyGoals(self):
 		''' Delete all AWR Optimizer Goals and apply the goals in self.optGoals
 		to the AWR project '''
 
 		# Clear prior goals
-		awrde.Project.OptGoals.RemoveAll()
+		self.awrde.Project.OptGoals.RemoveAll()
 
 		# Loop over all goals and apply them
 		for g in self.optGoals:
 			# Add goal
-			awrde.Project.OptGoals.AddGoal(g.circSimName, g.measName, g.comparison, g.w, g.L, g.xStart, g.xStop, g.xUnit, g.yStart, g.yStop, g.yUnit)
+			self.awrde.Project.OptGoals.AddGoal(g.circSimName, g.measName, g.comparison, g.w, g.L, g.xStart, g.xStop, g.xUnit, g.yStart, g.yStop, g.yUnit)
 
 		print(f"{self.cs}Successfully applied {len(self.optGoals)} optimizer goals.")
-
-	def
